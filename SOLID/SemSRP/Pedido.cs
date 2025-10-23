306fe2087fb81513a1072cabb59f41962890d23b
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2025-10-23 14:19:29 -0300

    Criando a pasta e a classe que viola o princípio SRP

---
+++ SOLID/SemSRP/Pedido.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Net.Mail;
+
+
+namespace SOLID.SemSRP
+{
+    internal class Pedido
+    {
+        public long Quantidade { get; set; }
+        public DateTime Data { get; set; }
+
+        public void IncluirPedido()
+        {
+            try
+            {
+                // Lógica para incluir o pedido
+                // Após cloncluir, envia um e-mail
+                MailMessage mail = new MailMessage("EmailFrom", "EmailTo", "EmailSubject", "EmailBody");
+
+                this.EnviarEmailPedido(mail);
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"C:\dados\ErrorLog.txt", ex.ToString());
+
+            }
+        }
+
+        public void DeletePedido()
+        {
+            try
+            {
+                // Lógica para deletar o pedido
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"C:\dados\ErrorLog.txt", ex.ToString());
+            }
+        }
+
+        public void EnviarEmailPedido(MailMessage mail)
+        {
+            try
+            {
+                //Lógica para enviar e-mail
+
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"C:\dados\ErrorLog.txt", ex.ToString());
+            }
+        }
+        
+    }
+}
